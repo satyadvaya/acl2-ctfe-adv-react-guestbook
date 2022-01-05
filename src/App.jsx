6b8340d3551f62ffd0e0b1728067f25d45b81bad
satyadvaya <satyadvaya@gmail.com>
2022-01-05 02:36:50 -0800

    installed react-router-dom@5, created Login component in new Auth folder, and added Router, Switch, Login, and Route paths into App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,22 @@
 import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Login from './views/Auth/Login';
 import Home from './views/Home/Home';
 import Layout from './views/Layout/Layout';
 
 export default function App() {
   return (
-    <Layout>
-      <Home />
-    </Layout>
+    <Router>
+      <Layout>
+        <Switch>
+          <Route path="/login">
+            <Login />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </Layout>
+    </Router>
   );
 }
--- src/App.test.jsx
+++
@@ -1,9 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
-import React from 'react';
-
-it('should render guest book', () => {
-  // render(<App />);
-  // const guestEntry = screen.getByText(/Guest Entry/i);
-  // expect(guestEntry).toBeInTheDocument();
-});
